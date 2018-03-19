0f87e17e10abab605b5057a7c0cfa79c1d3700a5
matrix-yang <739260804@qq.com>
2018-03-19 14:29:00 +0800

    first commit

---
+++ KNN/KNN.py
@@ -0,0 +1,116 @@
+#!/usr/bin/env python
+# coding: utf-8
+'''
+Created on Sep 16, 2010
+Update  on 2017-05-18
+@author: Peter Harrington/羊三/小瑶
+《机器学习实战》更新地址：https://github.com/apachecn/MachineLearning
+'''
+from numpy import *
+# 导入科学计算包numpy和运算符模块operator
+import operator
+from os import listdir
+from collections import Counter
+
+
+def createDataSet():
+    """
+    创建数据集和标签
+     调用方式
+     import kNN
+     group, labels = kNN.createDataSet()
+    """
+    group = array([[1.0, 1.1], [1.0, 1.0], [0, 0], [0, 0.1]])
+    labels = ['A', 'A', 'B', 'B']
+    return group, labels
+
+
+def classify0(inX, dataSet, labels, k):
+    """
+    inx[1,2,3]
+    DS=[[1,2,3],[1,2,0]]
+    inX: 用于分类的输入向量
+    dataSet: 输入的训练样本集
+    labels: 标签向量
+    k: 选择最近邻居的数目
+    注意：labels元素数目和dataSet行数相同；程序使用欧式距离公式.
+    预测数据所在分类可在输入下列命令
+    kNN.classify0([0,0], group, labels, 3)
+    """
+
+    # -----------实现 classify0() 方法的第一种方式----------------------------------------------------------------------------------------------------------------------------
+    # 1. 距离计算
+    dataSetSize = dataSet.shape[0]
+    # tile生成和训练样本对应的矩阵，并与训练样本求差
+    """
+    tile: 列-3表示复制的行数， 行-1／2表示对inx的重复的次数
+    In [8]: tile(inx, (3, 1))
+    Out[8]:
+    array([[1, 2, 3],
+        [1, 2, 3],
+        [1, 2, 3]])
+    In [9]: tile(inx, (3, 2))
+    Out[9]:
+    array([[1, 2, 3, 1, 2, 3],
+        [1, 2, 3, 1, 2, 3],
+        [1, 2, 3, 1, 2, 3]])
+    """
+    diffMat = tile(inX, (dataSetSize, 1)) - dataSet
+    """
+    欧氏距离： 点到点之间的距离
+       第一行： 同一个点 到 dataSet的第一个点的距离。
+       第二行： 同一个点 到 dataSet的第二个点的距离。
+       ...
+       第N行： 同一个点 到 dataSet的第N个点的距离。
+    [[1,2,3],[1,2,3]]-[[1,2,3],[1,2,0]]
+    (A1-A2)^2+(B1-B2)^2+(c1-c2)^2
+    """
+    # 取平方
+    sqDiffMat = diffMat ** 2
+    # 将矩阵的每一行相加
+    sqDistances = sqDiffMat.sum(axis=1)
+    # 开方
+    distances = sqDistances ** 0.5
+    sortedDistIndicies = distances.argsort()
+    classCount = {}
+    for i in range(k):
+        voteIlabel = labels[sortedDistIndicies[i]]
+        classCount[voteIlabel] = classCount.get(voteIlabel, 0) + 1
+    sortedClassCount = sorted(classCount.items(), key=operator.itemgetter(1), reverse=True)
+    return sortedClassCount[0][0]
+
+def file2matrix(filename):
+    """
+    导入训练数据
+    :param filename: 数据文件路径
+    :return: 数据矩阵returnMat和对应的类别classLabelVector
+    """
+    fr = open(filename)
+    # 获得文件中的数据行的行数
+    numberOfLines = len(fr.readlines())
+    # 生成对应的空矩阵
+    # 例如：zeros(2，3)就是生成一个 2*3的矩阵，各个位置上全是 0
+    returnMat = zeros((numberOfLines, 3))  # prepare matrix to return
+    classLabelVector = []  # prepare labels return
+    fr = open(filename)
+    index = 0
+    for line in fr.readlines():
+        # str.strip([chars]) --返回移除字符串头尾指定的字符生成的新字符串
+        line = line.strip()
+        # 以 '\t' 切割字符串
+        listFromLine = line.split('\t')
+        # 每列的属性数据
+        returnMat[index, :] = listFromLine[0:3]
+        # 每列的类别数据，就是 label 标签数据
+        classLabelVector.append(int(listFromLine[-1]))
+        index += 1
+    # 返回数据矩阵returnMat和对应的类别classLabelVector
+    return returnMat, classLabelVector
+
+import matplotlib
+import matplotlib.pyplot as plt
+fig = plt.figure()
+ax = fig.add_subplot(111)
+datingDataMat,datingLabels=file2matrix('datingTestSet2.txt')
+ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0*array(datingLabels), 15.0*array(datingLabels))
+plt.show()
