c1e8ee1027dcbf794cbc3496fae94d17a1085932
matrix-yang <739260804@qq.com>
2018-03-20 16:18:05 +0800

    加手写数字的图像

--- KNN/KNN.py
+++ KNN/KNN.py
@@ -132,12 +132,55 @@ def autoNorm(dataSet):
     # 将最小值之差除以范围组成矩阵
     normDataSet = normDataSet / tile(ranges, (m, 1))  # element wise divide
     return normDataSet, ranges, minVals
+
+
+def datingClassTest():
+    """
+    Desc:
+        对约会网站的测试方法
+    parameters:
+        none
+    return:
+        错误数
+    """
+    # 设置测试数据的的一个比例（训练数据集比例=1-hoRatio）
+    hoRatio = 0.1  # 测试范围,一部分测试一部分作为样本
+    # 从文件中加载数据
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')  # load data setfrom file
+    # 归一化数据
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    # m 表示数据的行数，即矩阵的第一维
+    m = normMat.shape[0]
+    # 设置测试的样本数量， numTestVecs:m表示训练样本的数量
+    numTestVecs = int(m * hoRatio)
+    print ('numTestVecs=', numTestVecs)
+    errorCount = 0.0
+    for i in range(numTestVecs):
+        # 对数据测试
+        classifierResult = classify0(normMat[i, :], normMat[numTestVecs:m, :], datingLabels[numTestVecs:m], 3)
+        print ("the classifier came back with: %d, the real answer is: %d" % (classifierResult, datingLabels[i]))
+        if (classifierResult != datingLabels[i]): errorCount += 1.0
+    print ("the total error rate is: %f" % (errorCount / float(numTestVecs)))
+    print (errorCount)
+
+def classifyPerson():
+    resultList = ['not at all', 'in small doses', 'in large doses']
+    percentTats = float(input("percentage of time spent playing video games ?"))
+    ffMiles = float(input("frequent filer miles earned per year?"))
+    iceCream = float(input("liters of ice cream consumed per year?"))
+    datingDataMat, datingLabels = file2matrix('datingTestSet2.txt')
+    normMat, ranges, minVals = autoNorm(datingDataMat)
+    inArr = array([ffMiles, percentTats, iceCream])
+    classifierResult = classify0((inArr-minVals)/ranges,normMat,datingLabels, 3)
+    print ("You will probably like this person: ", resultList[classifierResult - 1])
 # import matplotlib
 # import matplotlib.pyplot as plt
 # fig = plt.figure()
 # ax = fig.add_subplot(111)
-datingDataMat,datingLabels=file2matrix('datingTestSet2.txt')
+# datingDataMat,datingLabels=file2matrix('datingTestSet2.txt')
 # ax.scatter(datingDataMat[:, 1], datingDataMat[:, 2], 15.0*array(datingLabels), 15.0*array(datingLabels))
 # plt.show()
-normDataSet, ranges, minVals=autoNorm(datingDataMat);
-print(ranges)
+# normDataSet, ranges, minVals=autoNorm(datingDataMat);
+# print(ranges)
+# datingClassTest()
+classifyPerson()
