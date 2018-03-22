ddf6eb58785a8138ac8c1d47067b32810096194d
matrix-yang <739260804@qq.com>
2018-03-22 11:36:32 +0800

    注释代码

--- toolStudy/PILstudy/PILstudy.py
+++ toolStudy/PILstudy/PILstudy.py
@@ -3,15 +3,20 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def changeShape(arr, rr, rc):
+    #合并行后的中间矩阵
     inrm = np.zeros((rr, arr.shape[1]))
+    #结果矩阵
     rm = np.zeros((rr, rc))
+    #行列的缩小比例
     rratio = int(np.floor(arr.shape[0] / rr))
     cratio = int(np.floor(arr.shape[1] / rc))
+    #合并行
     for j in range(rr):
         temp = arr[j * rratio:(j + 1) * rratio, :]
         temp = temp.sum(axis=0)
         for i in range(arr.shape[1]):
             inrm[j, i] = temp[i]
+    #合并列
     for j in range(rc):
         temp = inrm[:, j * cratio:(j + 1) * cratio]
         temp = temp.sum(axis=1)
