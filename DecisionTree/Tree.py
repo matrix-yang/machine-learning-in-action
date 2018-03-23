7cd80cc492903c9368e67245217db8a14aada4d9
matrix-yang <739260804@qq.com>
2018-03-23 14:14:52 +0800

    新增决策树算法（信息增益）

---
+++ DecisionTree/Tree.py
@@ -0,0 +1,123 @@
+#!/usr/bin/env python
+# coding: utf-8
+from math import log
+
+def createDataSet():
+    dataSet = [[1, 1, 'yes'],
+            [1, 1, 'yes'],
+            [1, 0, 'no'],
+            [0, 1, 'no'],
+            [0, 1, 'no']]
+    labels = ['no surfacing', 'flippers']
+    return dataSet, labels
+
+
+def splitDataSet(dataSet, index, value):
+    """splitDataSet(通过遍历dataSet数据集，求出index对应的colnum列的值为value的行)
+        就是依据index列进行分类，如果index列的数据等于 value的时候，就要将 index 划分到我们创建的新的数据集中
+    Args:
+        dataSet 数据集                 待划分的数据集
+        index 表示每一行的index列        划分数据集的特征
+        value 表示index列对应的value值   需要返回的特征的值。
+    Returns:
+        index列为value的数据集【该数据集需要排除index列】
+    """
+    retDataSet = []
+    for featVec in dataSet:
+        # index列为value的数据集【该数据集需要排除index列】
+        # 判断index列的值是否为value
+        if featVec[index] == value:
+            # chop out index used for splitting
+            # [:index]表示前index行，即若 index 为2，就是取 featVec 的前 index 行
+            reducedFeatVec = featVec[:index]
+            '''
+            请百度查询一下： extend和append的区别
+            music_media.append(object) 向列表中添加一个对象object
+            music_media.extend(sequence) 把一个序列seq的内容添加到列表中 (跟 += 在list运用类似， music_media += sequence)
+            1、使用append的时候，是将object看作一个对象，整体打包添加到music_media对象中。
+            2、使用extend的时候，是将sequence看作一个序列，将这个序列和music_media序列合并，并放在其后面。
+            music_media = []
+            music_media.extend([1,2,3])
+            print music_media
+            #结果：
+            #[1, 2, 3]
+
+            music_media.append([4,5,6])
+            print music_media
+            #结果：
+            #[1, 2, 3, [4, 5, 6]]
+
+            music_media.extend([7,8,9])
+            print music_media
+            #结果：
+            #[1, 2, 3, [4, 5, 6], 7, 8, 9]
+            '''
+            reducedFeatVec.extend(featVec[index + 1:])
+            # [index+1:]表示从跳过 index 的 index+1行，取接下来的数据
+            # 收集结果值 index列为value的行【该行需要排除index列】
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+def calcShannonEnt(dataSet):
+    # 求list的长度，表示计算参与训练的数据量
+    numEntries = len(dataSet)
+    # 计算分类标签label出现的次数
+    labelCounts = {}
+    # the the number of unique elements and their occurrence
+    for featVec in dataSet:
+        # 将当前实例的标签存储，即每一行数据的最后一个数据代表的是标签
+        currentLabel = featVec[-1]
+        # 为所有可能的分类创建字典，如果当前的键值不存在，则扩展字典并将当前键值加入字典。每个键值都记录了当前类别出现的次数。
+        if currentLabel not in labelCounts.keys():
+            labelCounts[currentLabel] = 0
+        labelCounts[currentLabel] += 1
+
+    # 对于 label 标签的占比，求出 label 标签的香农熵
+    shannonEnt = 0.0
+    for key in labelCounts:
+        # 使用所有类标签的发生频率计算类别出现的概率。
+        prob = float(labelCounts[key])/numEntries
+        # 计算香农熵，以 2 为底求对数
+        shannonEnt -= prob * log(prob, 2)
+    return shannonEnt
+
+def chooseBestFeatureToSplit(dataSet):
+    """chooseBestFeatureToSplit(选择最好的特征)
+
+    Args:
+        dataSet 数据集
+    Returns:
+        bestFeature 最优的特征列
+    """
+    # 求第一行有多少列的 Feature, 最后一列是label列嘛
+    numFeatures = len(dataSet[0]) - 1
+    # 数据集的原始信息熵
+    baseEntropy = calcShannonEnt(dataSet)
+    # 最优的信息增益值, 和最优的Featurn编号
+    bestInfoGain, bestFeature = 0.0, -1
+    # iterate over all the features
+    for i in range(numFeatures):
+        # create a list of all the examples of this feature
+        # 获取对应的feature下的所有数据
+        featList = [example[i] for example in dataSet]
+        # get a set of unique values
+        # 获取剔重后的集合，使用set对list数据进行去重
+        uniqueVals = set(featList)
+        # 创建一个临时的信息熵
+        newEntropy = 0.0
+        # 遍历某一列的value集合，计算该列的信息熵
+        # 遍历当前特征中的所有唯一属性值，对每个唯一属性值划分一次数据集，计算数据集的新熵值，并对所有唯一特征值得到的熵求和。
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            # 计算概率
+            prob = len(subDataSet)/float(len(dataSet))
+            # 计算信息熵
+            newEntropy += prob * calcShannonEnt(subDataSet)
+        # gain[信息增益]: 划分数据集前后的信息变化， 获取信息熵最大的值
+        # 信息增益是熵的减少或者是数据无序度的减少。最后，比较所有特征中的信息增益，返回最好特征划分的索引值。
+        infoGain = baseEntropy - newEntropy
+        print ('infoGain=', infoGain, 'bestFeature=', i, baseEntropy, newEntropy)
+        if (infoGain > bestInfoGain):
+            bestInfoGain = infoGain
+            bestFeature = i
+    return bestFeature
--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # machine-learning-in-action
 study machine learning
-这是一个私人练习的代码库，主要涉及machine-learning-in-action中的列子
-已实现的算法
-KNN  ----------正在实现
+这是一个私人练习的代码库，主要涉及machine-learning-in-action中的列子已实现的算法
+
+KNN
+决策树----------正在实现
